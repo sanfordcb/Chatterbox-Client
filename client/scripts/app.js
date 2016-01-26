b33f828902f88015e1c344501999f39280d637db
sanfordcb <sanford0518@gmail.com>
2016-01-26 13:04:47 -0600

    Implement User Interface allowing users to select a username, add that user as a friend, and send a message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-
 var server = 'https://api.parse.com/1/classes/chatterbox';
 
 var cleanText = function(text) {
@@ -20,6 +19,36 @@ var cleanText = function(text) {
   return textArray.join('');
 };
 
+var newMessageBox = function(username) {
+  $('#popup').fadeIn();
+  $('textarea').attr('placeholder', 'Type a message to ' + username + ' and/or add ' + username + ' as a friend');
+  $('textarea').val('');
+  $('.addFriend').attr('checked', false);
+  $('#popup span').text(username);
+  $('#main').fadeOut();
+  $('#popup').append($('<p />', {html: "Close", class: 'closeButton'}));
+  $('#submit').click(function() {
+    var friendChecked = $('.addFriend').prop('checked');
+    var messageContent = $('textarea').val();
+    processSubmitForm(username, friendChecked, messageContent);
+  });
+  $('.closeButton').click(function() {
+    $('#popup').fadeOut();
+    $('#main').fadeIn();
+    $(this).remove();
+  });
+}
+
+var processSubmitForm = function(username, friendChecked, messageContent) {
+  $('#submit').unbind('click');
+  $('#popup').fadeOut();
+  $('#main').fadeIn();
+  $('.closeButton').remove();
+  console.log(username);
+  console.log(friendChecked);
+  console.log(messageContent);
+};
+
 var refreshMessages = function() {
     $.get(server, function(data) {
     $('#main').append($('<div id="messageList">'));
@@ -31,7 +60,7 @@ var refreshMessages = function() {
                                 $('<p />', {html: cleanText(item.roomname), class: 'room'}),
                                 $('<p />', {html: cleanText(item.createdAt), class: 'createdAt'}));
       $('.username').first().click(function() {
-  console.log('test');
+        newMessageBox(item.username);
 });
     });
   });
