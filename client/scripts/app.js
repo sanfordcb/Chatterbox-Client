e1a6675f48ffb37040c266bbb4771201ff60248f
sanfordcb <sanford0518@gmail.com>
2016-01-25 18:39:50 -0600

    Implement message retrieval from server, Implement refresh of messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,19 @@
-// YOUR CODE HERE:
+var server = 'https://api.parse.com/1/classes/chatterbox';
+
+var refreshMessages = function() {
+    $.get(server, function(data) {
+    $('#main').append($('<div id="messageList">'));
+    var $messageList = $('#messageList');
+    _.each(data.results, function(item) {
+      $messageList.prepend($('<div class="message">'));
+      $('.message').first().append($('<p />', {html: item.username, class: 'username'}),
+                                $('<p />', {html: item.text, class: 'text'}),
+                                $('<p />', {html: item.roomname, class: 'room'}),
+                                $('<p />', {html: item.createdAt, class: 'createdAt'}));
+    });
+  });
+};
+
+refreshMessages();
+setTimeout(refreshMessages, 15000);
 
