2427efcf5377302a39ab265d4dde7a1c1cf7e34c
sanfordcb <sanford0518@gmail.com>
2016-01-25 18:48:05 -0600

    Implement function to ensure proper html escaping

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,32 @@
 var server = 'https://api.parse.com/1/classes/chatterbox';
 
+var cleanText = function(text) {
+  text = text || '';
+  var textArray = text.split('');
+  var badChars = ['<', '>', '&', '"', "'", ' ', '!', '@',
+                  '$', '%', '(', ')', '=', '+', '{', '}',
+                  '[', ']', '-'];
+
+  for (var i = 0; i < badChars.length; i++) {
+    for (var j= 0; j < textArray.length; j++) {
+      if (textArray[j] === badChars[i]) {
+        textArray[j] = '&#' + textArray[j].charCodeAt(0) + ';';
+      }
+    }
+  }
+  return textArray.join('');
+};
+
 var refreshMessages = function() {
     $.get(server, function(data) {
     $('#main').append($('<div id="messageList">'));
     var $messageList = $('#messageList');
     _.each(data.results, function(item) {
       $messageList.prepend($('<div class="message">'));
-      $('.message').first().append($('<p />', {html: item.username, class: 'username'}),
-                                $('<p />', {html: item.text, class: 'text'}),
-                                $('<p />', {html: item.roomname, class: 'room'}),
-                                $('<p />', {html: item.createdAt, class: 'createdAt'}));
+      $('.message').first().append($('<p />', {html: cleanText(item.username), class: 'username'}),
+                                $('<p />', {html: cleanText(item.text), class: 'text'}),
+                                $('<p />', {html: cleanText(item.roomname), class: 'room'}),
+                                $('<p />', {html: cleanText(item.createdAt), class: 'createdAt'}));
     });
   });
 };
